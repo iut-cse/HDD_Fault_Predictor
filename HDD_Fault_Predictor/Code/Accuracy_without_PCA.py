e27d37d939796ff0cd3afea573a4ad8fc82728e8
Ela Zaman <raziazaman@iut-dhaka.edu>
2020-09-17 20:11:17 +0600

    updated

--- HDD_Fault_Predictor/Code/Accuracy_without_PCA.py
+++ HDD_Fault_Predictor/Code/Accuracy_without_PCA.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.naive_bayes import GaussianNB
+import classification as clf_2
 def without_pca():
     df=pd.read_csv('../Data/Final.csv')
     del df['serial_number']
@@ -11,7 +12,9 @@ def without_pca():
     y = df.iloc[:, -1]
     clf=GaussianNB()
     clf.fit(X,y)
-    print('Accuracy without PCA: ',clf.score(X,y))
+    print('Accuracy without PCA: ')
+    print('Naive Bayes:         ',clf.score(X,y)*100,' %')
+    print('Logistic Regression: ',clf_2.classify(X,y)*100,' %')
 
 
 without_pca()
